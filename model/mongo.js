bbf9295cddb1f7ac4bfc3ef051115f82a5f97c19
n1c01a5 <contact@wagner-nicolas.com>
2016-12-18 14:57:19 +0100

    add usecase in readme

--- model/mongo.js
+++ model/mongo.js
@@ -1,11 +1,11 @@
 var mongoose    =   require("mongoose");
-mongoose.connect('mongodb://localhost:27017/demoDb');
+mongoose.connect('mongodb://localhost:27017/ethercourt');
 // create instance of Schema
 var mongoSchema =   mongoose.Schema;
 // create schema
-var userSchema  = {
-    "userEmail" : String,
-    "userPassword" : String
+var ethercourtSchema  = {
+    "adressUser" : String,
+    "adressContract" : String
 };
 // create model if not exists.
-module.exports = mongoose.model('userLogin',userSchema);
+module.exports = mongoose.model('ethercourt', ethercourtSchema);
