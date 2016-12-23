7db25e057fa7750b2215d4fd4eeea024a94f5f77
root <root@ethercourt.localdomain>
2016-12-23 14:13:22 +0000

    add entrypoint twoPartyArbitrable

--- server.js
+++ server.js
@@ -7,51 +7,70 @@ var mongoOp     =   require("./model/mongo");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({"extended" : false}));
 
-// function isContract(addr) {
-//   let ethercourtContract = web3.eth.contract(contract.abi);
-//   let ethercourtContractInstance = ethercourtContract.at(addr);
-// }
+var cors = require('cors');
 
-router.get("/",function(req,res){
-    res.json({"error" : false, "message" : "It's works !"});
-});
-
-router.route("/users")
-    .get(function(req,res){
-        var response = {};
-        mongoOp.find({},function(err,data){
-            if(err) {
-                response = {"error" : true,"message" : "Error fetching data"};
-            } else {
-                response = {"error" : false,"message" : data};
-            }
-            res.json(response);
-        });
-    })
-    .post(function(req,res){
-        var db = new mongoOp();
-        var response = {};
-
-        db.adressUser = req.body.adressUser;
-        db.adressContract =  req.body.adressContract;
-        if (true) { // contract exixts
-          db.save(function(err){
-          // save() will run insert() command of MongoDB.
-          // it will add new data in collection.
-              if(err) {
-                  response = {"error" : true,"message" : "Error adding data"};
-              } else {
-                  response = {"error" : false,"message" : "Data added"};
-              }
-              res.json(response);
-          });
-        }
+router.route("/twoPartyArbitrable/:adress_user")
+  .get(function(req, res) {
+    mongoOp.find({adressUser: req.params.adress_user}, function(err, contractsUser) {
+      if (err)
+        res.send(err);
+      res.json(contractsUser);
     });
+  });
+
+router.route("/twoPartyArbitrable")
+  .get(function(req,res) {
+    mongoOp.find({},function(err, contracts) {
+      if(err) 
+        res.send(err); 
+      res.json(contracts);
+    });
+  })
+  .post(function(req,res){
+    var db = new mongoOp();
+    var response = {};
+
+    db.adressUser = req.body.adressUser;
+    db.adressContract =  req.body.adressContract;
+    db.save(function(err){
+      if(err) {
+        response = {"error" : true,"message" : "Error adding data"};
+      } else {
+        response = {"error" : false,"message" : "Data added"};
+      }
+      res.json(response);
+    });
+  });
 
 // parse application/json
 app.use(bodyParser.json())
 
+app.use(cors({origin: 'http://localhost:3000'}));
 app.use('/',router);
+
+app.all('/*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
+app.use(function (req, res, next) {
+
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+});
 
 app.listen(3000);
 console.log("Listening to PORT 3000");
